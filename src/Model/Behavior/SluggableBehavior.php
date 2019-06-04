8bb2727ed800548aed54c456e74ff762f0634df5
Simon Thißen <simonthissen@simons-mbp.speedport.ip>
2019-06-04 11:44:01 +0200

    add compatibility with revisionable plugin

--- src/Model/Behavior/SluggableBehavior.php
+++ src/Model/Behavior/SluggableBehavior.php
@@ -28,6 +28,160 @@ class SluggableBehavior extends Behavior
         'overwrite'     => false,
         ];
 
+    protected function _generateSlugForGeneralEntity(Entity $entity) {
+        $config = $this->getConfig();
+
+        if(!$entity->isNew() && !empty($entity->get($config['field']))) {
+            return $entity->get($config['field']);
+        }
+
+        // i want to create the following slug.
+        $defaultSlugName = Slug::generate($config['pattern'], $entity, $config['replacement']);
+
+        // checks suffix already there, if so then skip
+        $suffix = str_replace( $defaultSlugName . '-', '', $entity->get( $config['field'] ) );
+
+        if (is_numeric($suffix)) {
+            return $entity->get($config['field']);
+        }
+
+        /// only add a suffix if the table has more then one entry with the same slug ///
+        $customWhere = [];
+
+        // customWhere in order to exclude itself from the count below
+        if(is_array($this->getTable()->getPrimaryKey()) ) {
+            foreach($this->getTable()->getPrimaryKey() as $pk) {
+                $customWhere[$pk] = $entity->get($pk);
+            }
+        } else {
+            $customWhere[ $this->getTable()->getPrimaryKey() ] = $entity->get($this->getTable()->getPrimaryKey());
+        }
+
+        $condition = [
+            $config['field'] => $defaultSlugName,
+            "NOT" => $customWhere
+        ];
+
+        $count = $this->getTable()
+            ->find()
+            ->where($condition)
+            ->count();
+
+        if ($count > 0) {
+
+            // count of rows with slug-field having a -number suffix
+            $maxCount = $this->getTable()
+                ->find()
+                ->select(
+                    [
+                        'slug_count' => 'REPLACE(' . $config['field'] . ', "' . $defaultSlugName . '-", "")'
+                    ]
+                )
+                ->where(
+                    [
+                        $config['field'] . ' RLIKE' => $defaultSlugName . '-[0-9]+'
+                    ]
+                )
+                ->order(
+                    [
+                        'slug_count' => 'ASC'
+                    ]
+                )
+                ->count();
+
+            // for same named slugs
+            // start counting from 2
+            // first: slug
+            // second: slug-2
+            if ($maxCount === 0) {
+                $defaultSlugName .= "-" . 2;
+            } else {
+                $defaultSlugName .= "-" . ($maxCount + 2);
+            }
+
+        }
+
+        return $defaultSlugName;
+    }
+
+    protected function _generateSlugForRevisionableEntity(Entity $entity) {
+        $config = $this->getConfig();
+
+        if(!$entity->isNew()) {
+            return $entity->get($config['field']);
+        }
+
+        // i want to create the following slug.
+        $defaultSlugName = Slug::generate($config['pattern'], $entity, $config['replacement']);
+
+        // checks suffix already there, if so then skip
+        $suffix = str_replace( $defaultSlugName . '-', '', $entity->get( $config['field'] ) );
+
+        if (is_numeric($suffix)) {
+            return $entity->get($config['field']);
+        }
+
+        // only add a suffix if the table has more then one entry with the same slug
+        $customWhere = [];
+
+        if(is_array($this->getTable()->getPrimaryKey()) ) {
+            foreach($this->getTable()->getPrimaryKey() as $pk) {
+                if($pk == "revision") continue;
+
+                $customWhere[$pk] = $entity->get($pk);
+            }
+        } else {
+            $customWhere[$this->getTable()->getPrimaryKey()] = $entity->get($this->getTable()->getPrimaryKey());
+        }
+
+        $condition = [
+            $config['field'] => $defaultSlugName,
+            "valid_until" => "0000-00-00 00:00:00",
+            "NOT" => $customWhere
+        ];
+
+        $count = $this->getTable()
+            ->find()
+            ->where($condition)
+            ->count();
+
+        if ($count > 0) {
+
+            // count of rows with slug-field having a -number suffix
+            $maxCount = $this->getTable()
+                ->find()
+                ->select(
+                    [
+                        'slug_count' => 'REPLACE(' . $config['field'] . ', "' . $defaultSlugName . '-", "")'
+                    ]
+                )
+                ->where(
+                    [
+                        $config['field'] . ' RLIKE' => $defaultSlugName . '-[0-9]+'
+                    ]
+                )
+                ->order(
+                    [
+                        'slug_count' => 'ASC'
+                    ]
+                )
+                ->count();
+
+            // for same named slugs
+            // start counting from 2
+            // first: slug
+            // second: slug-2
+            if ($maxCount === 0) {
+                $defaultSlugName .= "-" . 2;
+            } else {
+                $defaultSlugName .= "-" . ($maxCount + 2);
+            }
+
+        }
+
+        return $defaultSlugName;
+    }
+
     /**
      * Uses the configuration settings to generate a slug for the given $entity.
      * @param Entity $entity
@@ -35,31 +189,31 @@ class SluggableBehavior extends Behavior
      */
     private function _generateSlug(Entity $entity)
     {
-
         $config = $this->getConfig();                                  # load the config built by the instantiated behavior
-
         if ($entity->get($config['field']) && !$config['overwrite']) :    # if already set, and !overwrite
             return $entity->get($config['field']);                  # return existing
         endif;
 
-        $value = Slug::generate($config['pattern'], $entity, $config['replacement']);
-
-
-        return $value;                                              # return the slug
+        $usedTraits = class_uses($entity);
+        if(in_array('TecBase\Revisionable\RevisionEntityTrait', $usedTraits)) {
+            return $this->_generateSlugForRevisionableEntity($entity);
+        } else {
+            return $this->_generateSlugForGeneralEntity($entity);
+        }                                            # return the slug
     }
 
     /**
      * Before Saving the entity, slug it.
      * @param Event $event
      * @param Entity $entity
-     * @return void
+     * @return mixed
      */
     public function afterSave(Event $event, Entity $entity, $options)
     {
         $config = $this->getConfig();                                  # load the config built by the instantiated behavior
 
-        $original = $entity->get($config['field']);                 # manually store $original - wasn't working for some reason otherwise
 
+        $original = $entity->get($config['field']);                 # manually store $original - wasn't working for some reason otherwise
 
         $usedTraits = class_uses($entity);
 
@@ -73,7 +227,6 @@ class SluggableBehavior extends Behavior
             endif;
             return true;
         }
-
 
         $entity->set($config['field'], $this->_generateSlug($entity)); # set the slug
 
